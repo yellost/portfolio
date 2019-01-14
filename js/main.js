cc549001033867d1d7536f45fbc34e34072bfd99
Benoit Franck <benoitfranck201@yahoo.fr>
2019-01-14 08:47:05 +0100

    on progresse

--- js/main.js
+++ js/main.js
@@ -1,6 +1,31 @@
-// $("nav").each(function(){
-//     $(this).hover(function(){
-//         $("nav .menu").toggleClass("closed");
-//         $("nav .menu").toggleClass("open");
-//     })
+//-----------Menu--------------//
+    console.log($("section"));
+$(".menu").click(function() {
+    let target = "#"+this.dataset.target;
+        if ($(target).hasClass("closed")) {
+            if (!$("main").hasClass("pages")) {
+                $("main").addClass("pages");
+                $("header").addClass("pages");
+            }
+            $(".open").addClass("closed");
+            $(".open").removeClass("open");
+            $(target).addClass("open");
+            $(target).removeClass("closed");
+        }
+});
+
+// --------Click on closed sections---------- //
+// $("section").click(function() {
+//     console.log("click");
+    
+//         if ($(this).hasClass("closed")) {
+//             if (!$("main").hasClass("pages")) {
+//                 $("main").addClass("pages");
+//                 $("header").addClass("pages");
+//             }
+//             $(".open").addClass("closed");
+//             $(".open").removeClass("open");
+//             $(this).addClass("open");
+//             $(this).removeClass("closed");
+//         }
 // });
